b245dc7a4849cfe908a34de2a9203bdb31902f99
Stacy Kimberley Hawadi <stacyhawadi@icloud.com>
2024-12-30 14:45:37 +0000

    Add products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-@%2k$*7!sbkjy+p+7_+l74b36$67&2+qt@-5k(gfmcs97&r7m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-stacykimber-boutiqueado-55g394m0d4s.ws.codeinstitute-ide.net',
-'8000-stacykimber-boutiqueado-ustvfzn8ej5.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-stacykimber-boutiqueado-ustvfzn8ej5.ws.codeinstitute-ide.net']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-stacykimber-boutiqueado-ustvfzn8ej5.ws.codeinstitute-ide.net',
+]
 
 
 # Application definition
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
