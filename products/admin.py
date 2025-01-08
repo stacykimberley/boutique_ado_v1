c125ed0633cf9a11a72b54b9da53f1b0b2b20cab
Stacy Kimberley Hawadi <stacyhawadi@icloud.com>
2025-01-08 02:19:03 +0000

    Add products, views and templates

--- products/admin.py
+++ products/admin.py
@@ -15,7 +15,7 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
-class CategoryAdmin(admin.ModelAdmin):    
+class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
